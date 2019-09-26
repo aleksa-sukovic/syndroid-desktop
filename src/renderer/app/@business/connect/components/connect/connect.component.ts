eca935a2b9dbc3dc8474a7a62392dd1a903cde7e
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-09-26 09:25:59 +0200

    Updates listener initialisation in connect component

--- src/renderer/app/@business/connect/components/connect/connect.component.ts
+++ src/renderer/app/@business/connect/components/connect/connect.component.ts
@@ -1,6 +1,6 @@
 import * as QR from 'qrcode-generator';
 import { Router } from "@angular/router";
-import { Component, NgZone, OnInit } from '@angular/core';
+import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import { ElectronService } from '../../../../@global/services/electron.service';
 
 @Component({
@@ -8,7 +8,7 @@ import { ElectronService } from '../../../../@global/services/electron.service';
     templateUrl: './connect.component.html',
     styleUrls: ['./connect.component.scss']
 })
-export class ConnectComponent implements OnInit
+export class ConnectComponent implements OnInit, OnDestroy
 {
     public ip: string = '';
 
@@ -19,11 +19,20 @@ export class ConnectComponent implements OnInit
 
     public ngOnInit(): void
     {
-        this.electron.ipcRenderer.send('ip:get');
-        this.electron.ipcRenderer.send('user:is-connected');
         this.electron.ipcRenderer.on('ip:get', this.handleIPAddressFetchEvent.bind(this));
         this.electron.ipcRenderer.on('client:connect', this.handleClientConnected.bind(this));
         this.electron.ipcRenderer.on('user:is-connected', this.handleUserConnectedStatus.bind(this));
+        setTimeout(() => {
+            this.electron.ipcRenderer.send('ip:get');
+            this.electron.ipcRenderer.send('user:is-connected');
+        }, 50);
+    }
+
+    public ngOnDestroy(): void
+    {
+        this.electron.ipcRenderer.removeListener('ip:get', this.handleIPAddressFetchEvent);
+        this.electron.ipcRenderer.removeListener('client:connect', this.handleClientConnected);
+        this.electron.ipcRenderer.removeListener('user:is-connected', this.handleUserConnectedStatus);
     }
 
     protected handleIPAddressFetchEvent(event: Event, data: any)
